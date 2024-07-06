069962667a7621bc1524074d662e0324db0a52c6
Aditya Pandey <adityapandey3082004@gmail.com>
2024-07-06 16:52:45 +0530

    Logo style

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -6,23 +6,42 @@ export const Appbar = () => {
     const userId=localStorage.getItem("userId");
     const navigate=useNavigate()
 
-    return <div className="border-b flex justify-between px-10 py-4">
-        <Link to={'/blogs'} className="flex flex-col justify-center cursor-pointer">
-                Blogify
+    return (
+      <div className="border-b flex bg-slate-50 justify-between px-10 py-4">
+        <Link
+          to={"/blogs"}
+          className="flex flex-col justify-center cursor-pointer"
+        >
+          <div className="flex justify-center">
+            <span className="font-bold text-2xl">B</span>
+            <span className="text-2xl font-thin">logify</span>
+          </div>
         </Link>
         <div>
-            <Link to={`/publish`}>
-                <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">New</button>
-            </Link>
-           
-            <button type="button" onClick={()=>{
-                localStorage.removeItem('userId');
-                localStorage.removeItem('token');
-                localStorage.removeItem('userName');
-                navigate("/signin")
-            }} className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">Log Out</button>
-            
-            <Avatar authorId={userId || ""} size={"big"} name={userName || ""} />
+          <Link to={`/publish`}>
+            <button
+              type="button"
+              className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 "
+            >
+              New
+            </button>
+          </Link>
+
+          <button
+            type="button"
+            onClick={() => {
+              localStorage.removeItem("userId");
+              localStorage.removeItem("token");
+              localStorage.removeItem("userName");
+              navigate("/signin");
+            }}
+            className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 "
+          >
+            Log Out
+          </button>
+
+          <Avatar authorId={userId || ""} size={"big"} name={userName || ""} />
         </div>
-    </div>
+      </div>
+    );
 }
